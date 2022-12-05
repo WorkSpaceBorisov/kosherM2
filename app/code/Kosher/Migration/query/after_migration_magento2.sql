7fbf20bb73e0670b78d7dbdb10f09b374d5b7dfa
WorkSpaceBorisov <borisovradionwork@gmail.com>
2022-12-05 11:51:48 +0300

    Prepare multistore settings

--- app/code/Kosher/Migration/query/after_migration_magento2.sql
+++ app/code/Kosher/Migration/query/after_migration_magento2.sql
@@ -1,1 +1,5 @@
 UPDATE core_config_data set value = 0 where path = 'catalog/frontend/list_allow_all'
+update core_config_data set value = 'https://pesach.m2.kosher4u.eu pesach/' where path = "web/unsecure/base_url";
+update core_config_data set value = 'https://pesach.m2.kosher4u.eu pesach/' where path = "web/unsecure/base_link_url";
+update core_config_data set value = 'https://pesach.m2.kosher4u.eu pesach/' where path = "web/secure/base_url";
+update core_config_data set value = 'https://pesach.m2.kosher4u.eu pesach/' where path = "web/secure/base_link_url";
