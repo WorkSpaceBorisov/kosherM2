7bc4e62afaa48f776abb989bcf34c1cff0b50364
WorkSpaceBorisov <borisovradionwork@gmail.com>
2022-12-16 10:42:17 +0300

    Adjustment auto deploy

--- app/code/Kosher/Migration/query/cleanup-m1-db-for-dev.sql
+++ app/code/Kosher/Migration/query/cleanup-m1-db-for-dev.sql
@@ -1,3 +1,5 @@
 delete from catalog_eav_attribute where attribute_id = 121;
 delete from catalog_product_entity_int where attribute_id = 121;
 delete from eav_entity_attribute where attribute_id = 121;
+
+delete from catalog_product_entity where sku is NULL;
