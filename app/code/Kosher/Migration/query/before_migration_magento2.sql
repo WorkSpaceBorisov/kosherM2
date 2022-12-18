84e0391e65d8795fedfb7a26c9f643c596f54f90
WorkSpaceBorisov <borisovradionwork@gmail.com>
2022-12-18 12:38:42 +0300

    Create and enable Wine Store

--- app/code/Kosher/Migration/query/before_migration_magento2.sql
+++ app/code/Kosher/Migration/query/before_migration_magento2.sql
@@ -1,2 +1,2 @@
-SET GLOBAL innodb_buffer_pool_size=4294967296;
-SELECT @@innodb_buffer_pool_size/4096/4096/4096;
+SET GLOBAL innodb_buffer_pool_size=3221225472;
+SELECT @@innodb_buffer_pool_size/3072/3072/3072;
