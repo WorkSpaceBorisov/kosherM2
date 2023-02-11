f05a13272aa0013ce6f498506f860772673e0763
WorkSpaceBorisov <borisovradionwork@gmail.com>
2023-02-11 18:22:14 +0200

    Conver CSV Product file from M1

--- app/code/Kosher/ConvertProductCsvM1File/Controller/Adminhtml/Convert/Index.php
+++ app/code/Kosher/ConvertProductCsvM1File/Controller/Adminhtml/Convert/Index.php
@@ -5,9 +5,6 @@ namespace Kosher\ConvertProductCsvM1File\Controller\Adminhtml\Convert;
 
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
-use Magento\Framework\App\ResponseInterface;
-use Magento\Framework\Controller\ResultInterface;
-use Magento\Framework\View\Result\Page;
 use Magento\Framework\View\Result\PageFactory;
 
 class Index extends Action
@@ -31,10 +28,7 @@ class Index extends Action
 
     const ADMIN_RESOURCE = 'Kosher_ConvertProductCsvM1File::convert';
 
-    /**
-     * @return Page|ResultInterface|ResponseInterface
-     */
-    public function execute(): Page|ResultInterface|ResponseInterface
+    public function execute()
     {
         $resultPage = $this->resultPageFactory->create();
         $resultPage->getConfig()->getTitle()->prepend(__('Edit Product CSV file From M1'));
