74fdc07b503e9a9ebd7c04470f6810d9c5963c1a
Dmytro Levin <weblevin@gmail.com>
2023-04-20 18:48:04 +0300

    Update header, catalog and add quick view popup structure

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+    default: {
+        area: 'frontend',
+        name: 'Kosher/default',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+        ],
+        dsl: 'less'
+    }
+};
