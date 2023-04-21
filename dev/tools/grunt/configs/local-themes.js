da4a99b36680595b9a6f2f4a6fdb41ded060d095
Dmytro Levin <weblevin@gmail.com>
2023-04-21 23:08:04 +0300

    Add some parts of theme

--- dev/tools/grunt/configs/local-themes.js
+++ dev/tools/grunt/configs/local-themes.js
@@ -1,14 +1,11 @@
-'use strict';
+"use strict";
 
 module.exports = {
     default: {
         area: 'frontend',
         name: 'Kosher/default',
         locale: 'en_US',
-        files: [
-            'css/styles-m',
-            'css/styles-l',
-        ],
+        files: [ 'css/styles-m', 'css/styles-l' ],
         dsl: 'less'
-    }
-};
+    },
+}
