5dbf2798026d75495b29037a5de6da6ac22bb184
Max Alekseyev <malekseyev@gomage.com>
2023-05-10 22:32:25 +0300

    Code cleanup

---
+++ app/design/frontend/Kosher/default/web/mage/menu.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'jquery',
+    'matchMedia',
+    'mage/translate'
+], function ($, mediaCheck) {
+    'use strict';
+
+    console.log('My menu');
+
+});
