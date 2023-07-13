1d4831de3615e2f2c2c9974b115d14b612899191
ivankorovkin <helloivankor@gmail.com>
2023-07-13 22:24:11 +0300

    minicart revert changes

--- app/design/frontend/Kosher/default/Magento_Checkout/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    config: {
-        mixins: {
-            'Magento_Checkout/js/view/minicart': {
-                'Magento_Checkout/js/view/minicart-mixin': true
-            }
-        }
-    }
-};
