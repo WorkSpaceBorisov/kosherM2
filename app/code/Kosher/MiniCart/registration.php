940d3a4ad85c20fade1798ca1c06d6c0baaac984
radion <borisovradionwork@gmail.com>
2023-07-22 13:26:16 +0300

    review code

--- app/code/Kosher/MiniCart/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Kosher_MiniCart',
-    __DIR__
-);
--- app/code/Kosher/StoresConfiguration/Model/ResourceModel/Address/Attribute/Source/CountryWithWebsites.php
+++ app/code/Kosher/StoresConfiguration/Model/ResourceModel/Address/Attribute/Source/CountryWithWebsites.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Kosher\StoresConfiguration\Model\ResourceModel\Address\Attribute\Source;
 
