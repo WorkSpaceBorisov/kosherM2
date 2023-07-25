b195b725414ba0d898a04b129aea8c1e410ea7d2
ivankor <helloivankor@gmail.com>
2023-07-25 13:12:48 +0300

    https://trello.com/c/8hZ3tY6O/186-bug-%D0%B7%D0%B0%D0%BA%D1%80%D1%8B%D1%82%D0%B8%D0%B5-%D0%BC%D0%B8%D0%BD%D0%B8%D0%BA%D0%B0%D1%80%D1%82%D1%8B-%D0%BF%D1%80%D0%B8-%D0%BB%D1%8E%D0%B1%D0%BE%D0%BC-%D1%81%D0%BA%D1%80%D0%BE%D0%BB%D0%B5

--- app/design/frontend/Kosher/default/web/js/main.js
+++ app/design/frontend/Kosher/default/web/js/main.js
@@ -22,11 +22,18 @@ define([
     }
 
     // Detect cart close on scroll
-    $(window).bind('mousewheel DOMMouseScroll touchmove', () => {
-        if(cartBtn.hasClass('active')) {
-            let scroll = $(window).scrollTop();
-            if (scroll > 150) closeMinicart();
+    
+
+    let lastScrollTop = 0;
+    $(window).scroll(function (event) {
+        let st = $(this).scrollTop();
+        if (st > lastScrollTop) {
+            if (cartBtn.hasClass('active')) {
+                let scroll = $(window).scrollTop();
+                if (scroll > 150) closeMinicart();
+            }
         }
+        lastScrollTop = st;
     });
 
     $('#kosher_overlay').on('click', () => {
@@ -34,7 +41,7 @@ define([
     });
 
     // Close nav/search/account on minicart call if opened
-    cartBtn.on('click', function(e) {
+    cartBtn.on('click', function (e) {
         e.preventDefault();
         if (!cartBtn.hasClass('active')) {
             closeAccount();
@@ -73,8 +80,8 @@ define([
     let closeMenu = () => {
         const $html = $('html');
         const $nav = $('.page-header [data-action="navigation"]');
-        
-        if($html.hasClass('nav-opened')) {
+
+        if ($html.hasClass('nav-opened')) {
             $html.removeClass('nav-opened');
             $nav.slideToggle(300, 'swing');
             $nav.find('.expanded').removeClass('expanded').find('.submenu').slideUp(300);
@@ -101,7 +108,7 @@ define([
         closeMenu();
         closeMobileSearch();
     });
-    
+
     // Mediacheck
     mediaCheck({
         media: breakPoint,
